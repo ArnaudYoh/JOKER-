805df55a57e83d756c2014cc93b82be1f10e81cf
naunauyoh <arnaudyoh@gmail.com>
2016-02-21 04:34:00 +0000

    starting work on actual Joker keyboard

--- JQscript.js
+++ JQscript.js
@@ -1,3 +1,44 @@
+var getsuit = function(){
+	var target = $('.cardtable')
+	if(target.hasClass('.spade')){
+		return "♠"
+	} else if(target.hasClass('.club')){
+		return "♣"
+	} else if(target.hasClass('.heart')){
+		return "♥"
+	} else {
+		return "♦"
+	}; 
+}
+
+var getnumber = function(target){
+	for(i = 2; i<11; i++){
+		var tempclass = "."+i; 
+		if(target.hasClass(tempclass)){
+			return i; 
+		}
+	}
+	if (target.hasClass('.A')){
+		return "A"; 
+	} else if ((target).hasClass('.J')){
+		return "J"; 
+	} else if ((target).hasClass('.Q')){
+		return "Q"; 
+	} else if ((target).hasClass('.K')){
+		return "K"; 
+	}
+}
+
+
+var writecard = function(target) {
+	var suit = getsuit(); 
+	var card = getnumber(target); 
+	console.log(card); 
+	console.log(suit); 
+}
+
+
+
 var main = function(){
 	var to_append_to = $('.to_append'); 
 	for (var i = 1; i <= 13; i++) {
@@ -134,15 +175,15 @@ var main = function(){
 			to_append_to.empty();
 			for (var i = 1; i <= 13; i++) {
 				if (i === 1){
-					to_append_to.append('<td><table class="redcards cards"><tr><td>♦</td></tr><tr><td>A</td></tr></table><td>'); 
+					to_append_to.append('<td><table class="redcards cards A"><tr><td>♦</td></tr><tr><td>A</td></tr></table><td>'); 
 				} else if (i===11 ){
-					to_append_to.append('<td><table class="redcards cards"><tr><td>♦</td></tr><tr><td>J</td></tr></table><td>'); 
+					to_append_to.append('<td><table class="redcards cards J"><tr><td>♦</td></tr><tr><td>J</td></tr></table><td>'); 
 				} else if (i===12){
-					to_append_to.append('<td><table class="redcards cards"><tr><td>♦</td></tr><tr><td>Q</td></tr></table><td>'); 		
+					to_append_to.append('<td><table class="redcards cards Q"><tr><td>♦</td></tr><tr><td>Q</td></tr></table><td>'); 		
 				} else if (i===13){
-					to_append_to.append('<td><table class="redcards cards"><tr><td>♦</td></tr><tr><td>K</td></tr></table><td>'); 	
+					to_append_to.append('<td><table class="redcards cards K"><tr><td>♦</td></tr><tr><td>K</td></tr></table><td>'); 	
 				} else {
-					to_append_to.append('<td><table class="redcards cards"><tr><td>♦</td></tr><tr><td>'+i+'</td></tr></table><td>'); 
+					to_append_to.append('<td><table class="redcards cards '+i+'"><tr><td>♦</td></tr><tr><td>'+i+'</td></tr></table><td>'); 
 				}
 
 				to_append_to.append('<td><table class="emptycards"><tr><td> </td></tr></table></td>'); 
@@ -150,6 +191,12 @@ var main = function(){
 			};
 		}
 	}); 
+
+	$('.cards').click(function(event){ 
+		var target = $(this); 
+		writecard(target);
+	}); 
+
 }
 
 
